8691ef63b61c1f78734638a0c8fa54a53f4089f7
Julio Santos <julioc.9730@gmail.com>
2023-10-09 07:32:31 -0300

    Update commands.js

--- cypress/support/commands/commandsLogin.js
+++ cypress/support/commands/commandsLogin.js
@@ -1,6 +1,4 @@
-/// <reference types="cypress" />
-
-Cypress.Commands.add('login', (email, password) => {
+Cypress.Commands.add('login', (email, passwd) => {
     cy.intercept('**/api/events').as('user');
     cy.intercept('**/recaptcha/api2/**').as('recaptcha');
 
@@ -8,8 +6,8 @@ Cypress.Commands.add('login', (email, password) => {
     cy.wait('@recaptcha');
     cy.get('[id*="loginWithU"]').click();
 
-    cy.get('#inputEmail').type(`${Cypress.env(email)}`, { delay: 180 });
-    cy.get('#inputPassword').type(`${Cypress.env(password)}`, { delay: 180 }, { log: false });
+    cy.get('#inputEmail').type(email, { delay: 180 });
+    cy.get('#inputPassword').type(passwd, { delay: 180 }, { log: false });
     cy.get('[type=submit]').first().click();
 
     cy.wait('@user');
